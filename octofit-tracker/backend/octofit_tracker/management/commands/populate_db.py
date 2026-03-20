de9f445c70a79a3e1577642331751e3d619a5889
Leigh Cousins <45980356+SuperSoaked@users.noreply.github.com>
2026-03-20 11:54:02 +0000

    Add initial Django project structure with models, views, serializers, and settings

---
+++ octofit-tracker/backend/octofit_tracker/management/commands/populate_db.py
@@ -0,0 +1,67 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth import get_user_model
+from djongo import models
+
+class Team(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    class Meta:
+        app_label = 'octofit_tracker'
+
+class Activity(models.Model):
+    user = models.CharField(max_length=100)
+    activity_type = models.CharField(max_length=100)
+    duration = models.IntegerField()
+    class Meta:
+        app_label = 'octofit_tracker'
+
+class Leaderboard(models.Model):
+    user = models.CharField(max_length=100)
+    score = models.IntegerField()
+    class Meta:
+        app_label = 'octofit_tracker'
+
+class Workout(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    class Meta:
+        app_label = 'octofit_tracker'
+
+class Command(BaseCommand):
+    help = 'Populate the octofit_db database with test data'
+
+    def handle(self, *args, **kwargs):
+        User = get_user_model()
+        # Clear data
+        User.objects.all().delete()
+        Team.objects.all().delete()
+        Activity.objects.all().delete()
+        Leaderboard.objects.all().delete()
+        Workout.objects.all().delete()
+
+        # Create teams
+        marvel = Team.objects.create(name='Marvel')
+        dc = Team.objects.create(name='DC')
+
+        # Create users
+        ironman = User.objects.create_user(username='ironman', email='ironman@marvel.com', password='password')
+        batman = User.objects.create_user(username='batman', email='batman@dc.com', password='password')
+        superman = User.objects.create_user(username='superman', email='superman@dc.com', password='password')
+        captain = User.objects.create_user(username='captainamerica', email='cap@marvel.com', password='password')
+
+        # Create activities
+        Activity.objects.create(user='ironman', activity_type='Running', duration=30)
+        Activity.objects.create(user='batman', activity_type='Cycling', duration=45)
+        Activity.objects.create(user='superman', activity_type='Swimming', duration=60)
+        Activity.objects.create(user='captainamerica', activity_type='Rowing', duration=40)
+
+        # Create leaderboard
+        Leaderboard.objects.create(user='ironman', score=100)
+        Leaderboard.objects.create(user='batman', score=90)
+        Leaderboard.objects.create(user='superman', score=95)
+        Leaderboard.objects.create(user='captainamerica', score=85)
+
+        # Create workouts
+        Workout.objects.create(name='Hero HIIT', description='High intensity interval training for heroes')
+        Workout.objects.create(name='Power Lift', description='Strength training for super strength')
+
+        self.stdout.write(self.style.SUCCESS('octofit_db database populated with test data.'))
---
+++ octofit-tracker/backend/octofit_tracker/tests.py
@@ -0,0 +1,31 @@
+from django.test import TestCase
+from .models import User, Team, Activity, Leaderboard, Workout
+
+class UserModelTest(TestCase):
+    def test_create_user(self):
+        user = User.objects.create_user(username='testuser', password='testpass')
+        self.assertEqual(user.username, 'testuser')
+
+class TeamModelTest(TestCase):
+    def test_create_team(self):
+        user = User.objects.create_user(username='member', password='pass')
+        team = Team.objects.create(name='Test Team')
+        team.members.add(user)
+        self.assertIn(user, team.members.all())
+
+class ActivityModelTest(TestCase):
+    def test_create_activity(self):
+        user = User.objects.create_user(username='activityuser', password='pass')
+        activity = Activity.objects.create(user=user, activity_type='run', duration=30, date='2023-01-01')
+        self.assertEqual(activity.activity_type, 'run')
+
+class LeaderboardModelTest(TestCase):
+    def test_create_leaderboard(self):
+        team = Team.objects.create(name='Leaderboard Team')
+        leaderboard = Leaderboard.objects.create(team=team, score=100)
+        self.assertEqual(leaderboard.score, 100)
+
+class WorkoutModelTest(TestCase):
+    def test_create_workout(self):
+        workout = Workout.objects.create(name='Pushups', description='Do 20 pushups')
+        self.assertEqual(workout.name, 'Pushups')
